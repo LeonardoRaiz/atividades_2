450e80630dda332cf461ccfed7499068220d1c98
Léo Raiz <leonardoraiz@icloud.com>
2023-02-13 15:15:27 -0300

    Commit inicial

---
+++ AulaOPL/AulaOPL.ts
@@ -0,0 +1,11 @@
+namespace AulaOPL {
+    let teste: boolean = true;
+
+    if(teste == true)
+    {
+        console.log(`O teste é Verdadeiro`);
+    } else {
+        console.log(`O teste é falso`);
+        
+    }
+}
